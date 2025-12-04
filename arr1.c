1e8b5e44f2b8d83b641729391bd22ee17544b20e
sravan427 <sravanrasa427@gmail.com>
2025-12-04 16:07:43 +0530

    day-63

--- arr1.c
+++ arr1.c
@@ -1,22 +1,43 @@
+// #include<stdio.h>
+// int main(){
+//     int n,key,a[48],f=0;
+//     printf("enter how many no. do you want: \n");
+//     scanf("%d",&n);
+//     printf("enter no.'s: \n");
+//     for(int i=0;i<n;i++)
+//     {
+//         scanf("%d",&a[i]);
+//     }
+//     printf("\nenter a number to search: ");
+//     scanf("%d",&key);
+//     for(int i=0;i<n;i++){
+//         if(a[i]==key){
+//             printf("a[%d]",i);
+//             f=1; }    
+//     }
+//     if(f==0)
+//     printf("\n not found");
+//     return 0;
+// }
 #include<stdio.h>
-int main(){
-    int n,key,a[48],f=0;
-    printf("enter how many no. do you want: \n");
+int main()
+{
+    int n,a[48];
+    printf("enter no.'s do you want :");
     scanf("%d",&n);
-    printf("enter no.'s: \n");
+    printf("enter values: ");
     for(int i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
-    printf("\nenter a number to search: ");
-    scanf("%d",&key);
-    for(int i=0;i<n;i++){
-        if(a[i]==key){
-            printf("a[%d]",i);
-            f=1; }    
+    int b=a[0];
+    for(int i=0;i<n;i++)
+    {
+        if(b<a[i])
+        {
+            b=a[i];
+        }
     }
-    if(f==0)
-    printf("\n not found");
+    printf("\n max=%d",b);
     return 0;
-
 }
---
+++ porf.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<process.h>
+#include<stdlib.h>
+void main()
+{
+	char name[25],res[10],ans;
+	int rn,m1,m2,tot;
+	float avg;
+	FILE *fp;
+	fp=fopen("stud.txt","w");
+	if(fp==NULL)
+	{
+		printf("\n ERROR:Unable to open the file");
+		exit(1);
+	}
+	do
+	{
+		fflush(stdin);
+		printf("\n enter name,rn,m1,m2:\n");
+		scanf("%s%d%d%d",name,&rn,&m1,&m2);
+		fflush(stdin);
+		tot=m1+m2;
+		avg=tot/2.0;
+		if(m1>=35&&m2>=35)
+			strcpy(res,"PASS");
+		else
+			strcpy(res,"FAIL");
+		fprintf(fp,"%s\n%d\n%d\n%d\n%d\n%f\n%s\n",name,rn,m1,m2,tot,avg,res);
+		fflush(stdin);
+		printf("\n want to enter more records[y/n]:\n");
+		fflush(stdin);
+		scanf("%c",&ans);
+		fflush(stdin);
+	}while(ans=='Y'||ans=='y');
+	fclose(fp);
+}
+
